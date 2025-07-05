632fa6c06ccbb4bf9e942245307b9e182d7fb6ad
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-07-05 21:02:29 +0530

    jwt auth middleware created

---
+++ jwt.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+const jwtAuthMiddleware = (req, res, next)=>{
+    // Extract the jwt token from the req header
+    // jwt auth middleware
+    const token = req.headers.autorization.split(' ')[1];
+    if(!token) return res.staus(401).json({error: 'Unauthorized' });
+
+    try{
+        // Verify the JWT token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Attach user information to the request object
+        req.userJWTPayload = decoded //the decoded payload came we want to send it to server
+        next();
+    }catch(err){
+        console.error(err);
+        res.staus(401).json({error: 'Invalid token!'});
+    }
+
+}
+
+export default jwtAuthMiddleware;
